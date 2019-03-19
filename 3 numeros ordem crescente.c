0c72edb725e1eff7905c5dab1448394be4a8d255
Gabriel Sol <gabsolsilva@gmail.com>
2019-03-18 22:50:24 -0300

    Programa

---
+++ 3 numeros ordem crescente.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c, primeiro, segundo, terceiro;
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if (a <= b) {
+        if (a <= c) {
+            primeiro = a;
+            if (b <= c){
+            terceiro = c;
+            segundo = b;
+            }
+            else{
+            terceiro = b;
+            segundo = c;
+            }
+        }
+        else{
+            primeiro = c;
+            segundo = a;
+            terceiro = b;
+        }
+    }
+    else {
+        if (b <= c) {
+            primeiro = b;
+            }
+            if (c <= a){
+                segundo = c;
+                terceiro = a;
+            }
+            else{
+                segundo = a;
+                terceiro = c;
+            }
+        else{
+            terceiro = a;
+            segundo = b;
+            primeiro = c;
+        }
+    }
+    printf("%d\n", primeiro);
+    printf("%d\n", segundo);
+    printf("%d\n", terceiro);
+    
+    
+    
+return 0;
+}
